a8fd9ce891c308e38dd78df98a4dc2c521337430
IAmGigabyteSpeed <62113741+IAmGigabyteSpeed@users.noreply.github.com>
2025-03-14 21:18:06 +0700

    Create Product Details Page

--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -5,6 +5,7 @@ import {
   ScrollView,
   View,
   TouchableOpacity,
+  Image,
 } from "react-native";
 import React, { useEffect, useState, useMemo, useRef } from "react";
 import axios from "axios";
@@ -42,6 +43,14 @@ export type RootStackParamList = {
 type Props = NativeStackNavigationProp<RootStackParamList>;
 
 const Home = () => {
+  useEffect(() => {
+    const callProducts = async () => {
+      const result = await axios.get(`${API_URL}/products/`);
+      console.log(result.data);
+      setProducts(result.data);
+    };
+    callProducts();
+  }, []);
   const navigation = useNavigation<Props>();
   const [products, setProducts] = useState<Product[]>([]);
   return (
@@ -52,7 +61,44 @@ const Home = () => {
         style={styles.searchBar}
         placeholder="Search for Products"
       ></TextInput>
-      <ScrollView style={styles.scrollView}></ScrollView>
+      <ScrollView style={styles.scrollView}>
+        {products.map((product) => (
+          <TouchableOpacity
+            onPress={() =>
+              navigation.navigate("Product", { ProductId: product._id })
+            }
+          >
+            <View style={styles.productBox} key={product._id}>
+              <Image
+                source={{ uri: product.image }}
+                style={styles.productImage}
+              />
+              <Text>{product.name}</Text>
+              <Text>{product.price}</Text>
+            </View>
+          </TouchableOpacity>
+        ))}
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate("Transaction", { transactionId: 86 })
+          }
+        >
+          <Text>Go to Transaction</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate("Product", { ProductId: 123981023 })
+          }
+        >
+          <Text>Go to Product</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Products", { Category: "Games" })}
+        >
+          <Text>Go to Products</Text>
+        </TouchableOpacity>
+        <Link screen="Categories">Testing Categories</Link>
+      </ScrollView>
     </View>
   );
 };
@@ -73,10 +119,26 @@ const styles = StyleSheet.create({
   },
   searchBar: {
     borderWidth: 1,
+    borderRadius: 20,
     fontSize: 12,
+    paddingLeft: 5,
+    paddingRight: 5,
   },
   scrollView: {
     flex: 1,
     paddingTop: 10,
+    gap: 5,
+  },
+  productImage: {
+    height: 150,
+    width: 150,
+  },
+  productBox: {
+    width: "50%",
+    borderWidth: 1,
+    borderColor: "black",
+    borderRadius: 10,
+    padding: 10,
+    backgroundColor: "white",
   },
 });
--- app/screens/Product.tsx
+++ app/screens/Product.tsx
@@ -1,19 +1,116 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "./Home";
+import axios from "axios";
+import { API_URL } from "../context/AuthContext";
+
+interface Category {
+  _id: string;
+  name: string;
+  description: string;
+  createdAt: string;
+}
+
+interface Product {
+  _id: string;
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+  category: Category;
+  image: string;
+  createdAt: string;
+}
 
 type Props = NativeStackScreenProps<RootStackParamList, "Product">;
 
 const Product = ({ route, navigation }: Props) => {
+  const [product, setProduct] = useState<Product>();
   const { ProductId } = route.params;
+  useEffect(() => {
+    const callProducts = async () => {
+      const result = await axios.get(`${API_URL}/products/${ProductId}`);
+      console.log(result.data);
+      setProduct(result.data);
+    };
+    callProducts();
+  }, []);
   return (
-    <View>
-      <Text>Product {ProductId}</Text>
+    <View style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        <Image source={{ uri: product?.image }} style={styles.productImage} />
+        <Text style={styles.productTitle}>{product?.name}</Text>
+        <Text style={styles.productPrice}>Rp.{product?.price}</Text>
+        <Text>{product?.category.name}</Text>
+        <Text style={styles.productDescriptionTitle}>Description</Text>
+        <Text style={styles.productDescription}>{product?.description}</Text>
+        <Text>Quantity: {product?.stock}</Text>
+      </ScrollView>
+
+      <View style={styles.footer}>
+        <TouchableOpacity
+          style={{
+            padding: 10,
+            backgroundColor: "lightblue",
+            borderRadius: 5,
+            marginRight: 10,
+            width: "auto",
+          }}
+        >
+          <Text
+            style={{ color: "black", fontWeight: "bold", textAlign: "center" }}
+          >
+            + Add to Cart
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 export default Product;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f8f8f8",
+    fontFamily: "sans-serif",
+  },
+  scrollView: {
+    padding: 32,
+    paddingBottom: 80,
+  },
+  productImage: {
+    height: 300,
+    width: "auto",
+  },
+  productTitle: {
+    fontSize: 20,
+  },
+  productPrice: {
+    fontSize: 24,
+  },
+  productDescriptionTitle: {
+    fontSize: 18,
+  },
+  productDescription: {
+    fontSize: 16,
+  },
+  footer: {
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: "white",
+    padding: 15,
+    borderTopWidth: 1,
+    borderColor: "#ddd",
+  },
+});
