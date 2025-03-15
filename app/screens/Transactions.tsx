52e32c248e9bbc448fb472f6618dee6da8e21fda
IAmGigabyteSpeed <62113741+IAmGigabyteSpeed@users.noreply.github.com>
2025-03-15 20:57:30 +0700

    Implement get Transactions Page

--- app/screens/Transactions.tsx
+++ app/screens/Transactions.tsx
@@ -1,10 +1,43 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, ScrollView } from "react-native";
+import React, { useEffect, useState } from "react";
+import MainStyle from "../context/styles";
+import axios from "axios";
+import { API_URL, useAuth } from "../context/AuthContext";
+interface Products {
+  product: string;
+  quantity: number;
+}
+interface Transcations {
+  _id: string;
+  user: string;
+  products: Products;
+  totalAmount: number;
+  status: string;
+  createdAt: Date;
+}
 
 const Transactions = () => {
+  const { userInfo, authState } = useAuth();
+  const [Transactions, setTransactions] = useState<Transcations[]>([]);
+  useEffect(() => {
+    const getTransaction = async () => {
+      const result = await axios.get(`${API_URL}/transactions/${userInfo?.id}`);
+      console.log(result.data.message);
+      setTransactions(result.data);
+    };
+    getTransaction();
+  }, []);
   return (
-    <View>
+    <View style={MainStyle.container}>
       <Text>Transactions</Text>
+      <ScrollView contentContainerStyle={MainStyle.scrollView}>
+        {Transactions.map((Transaction) => (
+          <View key={Transaction._id}>
+            <Text>{Transaction._id}</Text>
+            <Text>{Transaction.user}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 };
